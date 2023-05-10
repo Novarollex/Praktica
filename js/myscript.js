e6366508ba9a47202e46b53643453a6b21ca9e27
Denis Mamontov <mister.mamontoff2016@yandex.ru>
2023-05-10 18:33:42 +0400

    Practica

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function changeImage() {
+        document.getElementById("imgClickAndChange").src = "../PIC/еда.png";
+}
+function changeImage1() {
+        document.getElementById("imgClickAndChange").src = "../PIC/напитки.png";
+}
+function changeImage2() {
+        document.getElementById("imgClickAndChange").src = "../PIC/на_вынос.png";
+}
